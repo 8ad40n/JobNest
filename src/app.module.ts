b82e6a70f59a08a205455146fe7040f303d92989
Badhon Nath Joy <joy184110@gmail.com>
2024-03-17 14:14:32 +0600

    AI added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import config from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ChatCompletionApiModule } from './chat-completion-api/chat-completion-api.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), AuthModule],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, ChatCompletionApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
