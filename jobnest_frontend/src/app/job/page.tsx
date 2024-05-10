f8876de051cfe53f47f79dfa8745e265e52a24a5
8ad40n <joy184110@gmail.com>
2024-05-10 23:18:28 +0600

    Data fetch of jobs successful

--- jobnest_frontend/src/app/job/page.tsx
+++ jobnest_frontend/src/app/job/page.tsx
@@ -1,8 +1,31 @@
+"use client"
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Job() {
+
+    const [myJobs, setMyJobs] = useState([])
+
+    useEffect(()=>{
+        axios.get("http://localhost:8000/job")
+        .then((res)=> setMyJobs(res.data));
+    },[])
   return (
     <main className="flex min-h-screen flex-col justify-between p-24 container mx-auto px-1 lg:px-20 md:px-10">
-
-        
+        <h1>All jobs:</h1>
+        {myJobs.map((post:any)=>{
+            const {jobID, title, description, budget, duration, date} = post;
+            return(
+                <div className="jobCard" key={jobID}>
+                <p>{jobID}</p>
+                <p>{title}</p>
+                <p>{description}</p>
+                <p>{budget}</p>
+                <p>{duration}</p>
+                <p>{date}</p>
+                </div>
+            )
+        })}
     </main>
   );
 }
